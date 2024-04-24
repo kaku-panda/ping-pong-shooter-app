01d2f7bc76eabdff528116a1f8bd97cd6133bc26
kaku-panda <program.kaku.panda@gmail.com>
2024-04-24 23:54:45 +0900

    ar_flutter_plugin から arklit_flutter に切り替え

--- lib/screens/ar.dart
+++ lib/screens/ar.dart
@@ -4,19 +4,11 @@
 
 import 'dart:async';
 
-import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
-import 'package:ar_flutter_plugin/datatypes/node_types.dart';
-import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
-import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
-import 'package:ar_flutter_plugin/models/ar_node.dart';
-import 'package:ar_flutter_plugin/widgets/ar_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_yolov5_app/main.dart';
 import 'package:vector_math/vector_math_64.dart';
+import 'package:arkit_plugin/arkit_plugin.dart';
 
 
 class AugmentedRearityScreen extends ConsumerStatefulWidget {
@@ -26,101 +18,23 @@ class AugmentedRearityScreen extends ConsumerStatefulWidget {
 }
 
 class AugmentedRearityScreenState extends ConsumerState<AugmentedRearityScreen> {
-  
-  Size screenSize = const Size(0, 0);
-
-  ARSessionManager? arSessionManager;
-  ARObjectManager? arObjectManager;
-  ARNode? localObjectNode;
-  ARNode? webObjectNode;
-  ARNode? fileSystemNode;
+  late ARKitController arkitController;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("AR"),
-      ),
-      body: Stack(
-        children: [
-          ARView(
-            key: ref.watch(arPluginProvider).arViewKey,
-            onARViewCreated: onARViewCreated,
-            planeDetectionConfig: PlaneDetectionConfig.horizontalAndVertical,
-          ),
-        ],
-      ),
-    );
-  }
-  
-  void onARViewCreated(
-    ARSessionManager arSessionManager,
-    ARObjectManager arObjectManager,
-    ARAnchorManager arAnchorManager,
-    ARLocationManager arLocationManager,
-  ) {
-    this.arSessionManager = arSessionManager;
-    this.arObjectManager = arObjectManager;
-
-    this.arSessionManager!.onInitialize(
-      showFeaturePoints: true,
-      showPlanes: true,
-      customPlaneTexturePath: "assets/images/triangle.png",
-      showWorldOrigin: true,
-      handleTaps: true,
-    );
-    this.arObjectManager!.onInitialize();
-
-    this.arSessionManager!.onPlaneOrPointTap = this.arSessionManager!.onPlaneOrPointTap = onPlaneOrPointTap;
+  void dispose() {
+    arkitController.dispose();
+    super.dispose();
   }
 
-  Future<void> onLocalObjectAtOriginButtonPressed() async {
-    if (localObjectNode != null) {
-      arObjectManager!.removeNode(localObjectNode!);
-      localObjectNode = null;
-    } else {
-      var newNode = ARNode(
-          type: NodeType.localGLTF2,
-          uri: "Models/Chicken_01/Chicken_01.gltf",
-          scale: Vector3(0.02, 0.02, 0.02),
-          position: Vector3(0.0, 0.0, 0.0),
-          rotation: Vector4(1.0, 0.0, 0.0, 0.0));
-      bool? didAddLocalNode = await arObjectManager!.addNode(newNode);
-      localObjectNode = (didAddLocalNode!) ? newNode : null;
-    }
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(title: const Text('ARKit in Flutter')),
+      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+    final node = ARKitNode(
+        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
+    this.arkitController.add(node);
   }
-
-  void onPlaneOrPointTap(List<ARHitTestResult> hitResults) {
-    if (hitResults.isNotEmpty) {
-      ARHitTestResult firstResult = hitResults.first;
-
-      // すでにノードが存在する場合は削除
-      if (localObjectNode != null) {
-        arObjectManager!.removeNode(localObjectNode!);
-        localObjectNode = null;
-      }
-
-      // 新しいノードを作成し、タップされた位置に配置
-      ARNode newNode = ARNode(
-        type: NodeType.localGLTF2,
-        uri: "Models/Chicken_01/Chicken_01.gltf",
-        scale: Vector3(0.02, 0.02, 0.02),  // スケールは適宜調整
-        position: firstResult.worldTransform.getTranslation(),  // タップした位置
-        rotation: Vector4(1.0, 0.0, 0.0, 0.0)  // 回転は適宜調整
-      );
-
-      // ノードをARセッションに追加
-      arObjectManager!.addNode(newNode).then((bool? didAddNode) {
-        if (didAddNode!) {
-          localObjectNode = newNode;  // 追加成功した場合は保存
-          print("モデルをタップ位置に配置しました");
-        } else {
-          print("モデルの配置に失敗しました");
-        }
-      });
-    } else {
-      print("タップ位置が無効です");
-    }
-  }
-
 }
